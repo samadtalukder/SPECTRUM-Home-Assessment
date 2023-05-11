5f1cfbd798e5cd93a16a842f03218f7c87f5a9f1
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-11 19:22:14 +0800

    Created Constants object file. Added ApiKey, BaseUrl and Timeout

---
+++ app/src/main/java/com/samad_talukder/spectrumassessment/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.samad_talukder.spectrumassessment.utils
+
+
+object Constants {
+    const val CONNECTION_TIMEOUT = 45
+    const val READ_TIMEOUT = 45
+    const val WRITE_TIMEOUT = 45
+    const val API_KEY = "0e7274f05c36db12cbe71d9ab0393d47"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+}
