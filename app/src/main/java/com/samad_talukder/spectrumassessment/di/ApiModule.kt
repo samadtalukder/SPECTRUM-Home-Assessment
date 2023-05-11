75d289a545c3bcef4254a7ba87297a4bc01c1ee3
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-11 20:00:49 +0800

    Created ApiModule For DI Added Api service, Base Url

---
+++ app/src/main/java/com/samad_talukder/spectrumassessment/di/ApiModule.kt
@@ -0,0 +1,69 @@
+package com.samad_talukder.spectrumassessment.di
+
+import com.samad_talukder.spectrumassessment.BuildConfig
+import com.samad_talukder.spectrumassessment.data.api.SpectrumApi
+import com.samad_talukder.spectrumassessment.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+
+        return loggingInterceptor
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(Constants.CONNECTION_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .readTimeout(Constants.READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .writeTimeout(Constants.WRITE_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): SpectrumApi =
+        retrofit.create(SpectrumApi::class.java)
+}
