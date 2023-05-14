9b2900e9a9e14c343b11264053f3c7b0aa3edc78
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-14 15:02:23 +0800

    Added BASE_IMAGE_URL and MOVIE_ID

--- app/src/main/java/com/samad_talukder/spectrumassessment/utils/Constants.kt
+++ app/src/main/java/com/samad_talukder/spectrumassessment/utils/Constants.kt
@@ -7,4 +7,8 @@ object Constants {
     const val WRITE_TIMEOUT = 45
     const val API_KEY = "0e7274f05c36db12cbe71d9ab0393d47"
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original"
+    const val MOVIE_ID = "movie_id"
+
+
 }
