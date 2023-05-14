1c8b7f76cc8fe081a048c49ed7e202f07d804ecd
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-14 15:04:51 +0800

    Update Result class instead of MovieItem

--- app/src/main/java/com/samad_talukder/spectrumassessment/domain/model/MovieResponse.kt
+++ app/src/main/java/com/samad_talukder/spectrumassessment/domain/model/MovieResponse.kt
@@ -3,7 +3,7 @@ package com.samad_talukder.spectrumassessment.domain.model
 data class MovieResponse(
     val dates: Dates,
     val page: Int,
-    val results: List<Result>,
+    val results: List<MovieItem>,
     val total_pages: Int,
     val total_results: Int
 )
