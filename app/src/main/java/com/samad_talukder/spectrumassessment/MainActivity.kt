8108ceadd30294e597ad44c291fb98cdb483eed5
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-14 15:03:57 +0800

    Added AndroidEntryPoint and ViewBinding

--- app/src/main/java/com/samad_talukder/spectrumassessment/MainActivity.kt
+++ app/src/main/java/com/samad_talukder/spectrumassessment/MainActivity.kt
@@ -2,10 +2,21 @@ package com.samad_talukder.spectrumassessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.samad_talukder.spectrumassessment.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
