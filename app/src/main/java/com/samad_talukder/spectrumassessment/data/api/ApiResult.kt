a7361bf5699ed91173d53870ed8ee5a2e34a96e7
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-15 12:25:16 +0800

    Update api result class

--- app/src/main/java/com/samad_talukder/spectrumassessment/data/api/ApiResult.kt
+++ app/src/main/java/com/samad_talukder/spectrumassessment/data/api/ApiResult.kt
@@ -1,15 +1,7 @@
 package com.samad_talukder.spectrumassessment.data.api
 
-sealed class ApiResult<out T> {
-
+sealed class ApiResult<out T : Any> {
     object Loading : ApiResult<Nothing>()
-
-    data class Success<out T>(
-        val data: T
-    ) : ApiResult<T>()
-
-    data class Error(
-        val errorMessage: String,
-        val code: Int,
-    ) : ApiResult<Nothing>()
+    data class Success<out T : Any>(val data: T) : ApiResult<T>()
+    data class Error(val errorMessage: String, val code: Int, ) : ApiResult<Nothing>()
 }
