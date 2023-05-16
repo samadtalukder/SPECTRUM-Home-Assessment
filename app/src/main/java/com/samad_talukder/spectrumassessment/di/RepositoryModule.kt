7355cf4e62440d3c6254d51acfb0a2d5e5976ad6
Samad Talukder <samadtalukdar6@gmail.com>
2023-05-17 03:12:18 +0800

    Added Favourite Repository

--- app/src/main/java/com/samad_talukder/spectrumassessment/di/RepositoryModule.kt
+++ app/src/main/java/com/samad_talukder/spectrumassessment/di/RepositoryModule.kt
@@ -1,7 +1,10 @@
 package com.samad_talukder.spectrumassessment.di
 
 
+import com.samad_talukder.spectrumassessment.data.local.dao.FavouritesDao
 import com.samad_talukder.spectrumassessment.data.remote.MovieDataSource
+import com.samad_talukder.spectrumassessment.data.repository.FavouriteRepository
+import com.samad_talukder.spectrumassessment.data.repository.FavouriteRepositoryImpl
 import com.samad_talukder.spectrumassessment.data.repository.MovieRepository
 import com.samad_talukder.spectrumassessment.data.repository.MovieRepositoryImpl
 import dagger.Module
@@ -18,5 +21,10 @@ class RepositoryModule {
     @Provides
     fun providesMovieRepository(movieDataSource: MovieDataSource): MovieRepository {
         return MovieRepositoryImpl(movieDataSource)
+    }
+    @Singleton
+    @Provides
+    fun providesFavouriteRepository(favouriteSource: FavouritesDao): FavouriteRepository {
+        return FavouriteRepositoryImpl(favouriteSource)
     }
 }
